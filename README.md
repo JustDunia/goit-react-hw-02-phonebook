b32ab54e07c857ffebfdcc7702558f8581eac4c8
Justyna Sienkiewicz <109360997+JustDunia@users.noreply.github.com>
2023-05-18 20:12:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# React HW 2 - feedback [LINK](https://justdunia.github.io/goit-react-hw-02-feedback2)
---
+++ src/components/Statistics.js
@@ -0,0 +1,25 @@
+import { Component } from 'react';
+import { PropTypes } from 'prop-types';
+
+export class Statistics extends Component {
+  render() {
+    const { good, neutral, bad, total, positive } = this.props;
+    return (
+      <div>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>Total: {total}</p>
+        <p>Positive feedback: {positive}%</p>
+      </div>
+    );
+  }
+}
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positive: PropTypes.number.isRequired,
+};
